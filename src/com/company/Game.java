0cdfdd3b8780faefeb5f495d9ce7592ae3262c9a
Bartłomiej Trawiński <bartraw23@gmail.com>
2020-03-24 18:21:20 +0100

    tested, works great

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -7,6 +7,7 @@ public class Game {
 
         char guessedLetter = ' ';
         boolean result = false;
+        boolean anyMatch =false;
         System.out.println("Welcome in the guessing game");
         //System.out.println("you are guessing: ");
         //System.out.println(displayedTitle);
@@ -26,24 +27,32 @@ public class Game {
                 boolean match =(guessedLetter==hiddenTitle[j]);
                 if (match){
                     displayedTitle[j]=hiddenTitle[j];
+                    anyMatch =true;
                 }
             }
 
-            //checking whole title
-            Scanner scanner1 = new Scanner(System.in);
-            System.out.println("Want to check whole title? [Y/N]");
-            String answer = scanner1.nextLine();
-            if (answer.equals("Y")){
-                System.out.println("Go on:");
-                Scanner scanner2 = new Scanner(System.in);
-                String titleGuessedString = scanner2.nextLine();
-                if (titleGuessedString.equals(title)){
-                    System.out.println("You have guessed!");
-                    result=true;
-                    break;
-                }else{
-                    System.out.println("Unluckily you are wrong!");
+            if (anyMatch){
+                //checking whole title
+                System.out.println("you guessed one letter!");
+                System.out.println(displayedTitle);
+                anyMatch =false;
+                Scanner scanner1 = new Scanner(System.in);
+                System.out.println("Want to check whole title? [y/!y]");
+                String answer = scanner1.nextLine();
+                if (answer.equals("y")){
+                    System.out.println("Go on:");
+                    Scanner scanner2 = new Scanner(System.in);
+                    String titleGuessedString = scanner2.nextLine();
+                    if (titleGuessedString.equals(title)){
+                        System.out.println("You have guessed!");
+                        result=true;
+                        break;
+                    }else{
+                        System.out.println("Unluckily you are wrong!");
+                    }
                 }
+            }else{
+                System.out.println("You didn't guess UNLUCKY");
             }
 
 
